7e2267f682327a8ebb95d8c6d056d7694af2f6d9
Nikita <KovaletzNikita@yandex.ru>
2017-10-23 16:20:21 +0500

    one more commit

--- bj/urls.py
+++ bj/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.home, name='home'),
 	url(r'^login/$', views.login, name='login'),
-	url(r'^registration$', views.registr, name='registr'),
+	url(r'^registration/$', views.registr, name='registr'),
 ]
